17c01952f18292a9b993a5c28bc8fb965b11f825
uji100 <97239721+uji100@users.noreply.github.com>
2023-08-03 11:46:22 +0530

    add book entity

---
+++ Part-01/src/main/java/software/ujithamigara/entity/Book.java
@@ -0,0 +1,45 @@
+package software.ujithamigara.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+    @Id
+    private Integer id;
+    private String title;
+    private String ISBN;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public Book() {
+    }
+
+    public Book(Integer id, String title, String ISBN) {
+        this.id = id;
+        this.title = title;
+        this.ISBN = ISBN;
+    }
+}
