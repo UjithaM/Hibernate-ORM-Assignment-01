420ba9f4d4cdebd1b66656a604eb4854153d4822
uji100 <97239721+uji100@users.noreply.github.com>
2023-08-03 12:06:19 +0530

    add FactoryConfiguration and implemented it

---
+++ Part-02/src/main/java/software/ujithamigara/ManyToMany/util/FactoryConfiguration.java
@@ -0,0 +1,25 @@
+package software.ujithamigara.ManyToMany.util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import software.ujithamigara.ManyToMany.entity.Author;
+import software.ujithamigara.ManyToMany.entity.Book;
+
+public class FactoryConfiguration {
+    private static FactoryConfiguration factoryConfiguration;
+    private final SessionFactory sessionFactory;
+    private FactoryConfiguration(){
+        Configuration configuration = new Configuration().configure().addAnnotatedClass(Book.class).addAnnotatedClass(Author.class);
+
+        sessionFactory = configuration.buildSessionFactory();
+
+    }
+    public static FactoryConfiguration getInstance(){
+        return (factoryConfiguration == null)?factoryConfiguration = new FactoryConfiguration() : factoryConfiguration;
+    }
+    public Session getSession(){
+        return sessionFactory.openSession();
+    }
+
+}
